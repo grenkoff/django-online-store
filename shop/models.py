d112d4b30b2761dbc3d18fbe561e8890f20911d4
Alexander Grenkov <a.grenkov@gmail.com>
2024-03-12 14:36:40 +0500

    add models

---
+++ shop/models.py
@@ -0,0 +1,96 @@
+from django.db import models
+from django.utils.text import slugify
+import random
+import string
+from django.urls import reverse
+
+
+def rand_slug():
+    """
+    Generates a random slug consisting of lowercase letters and digits.
+    """
+    return "".join(random.choice(string.ascii_lowercase + string.digits) for _ in range(3))
+
+class Category(models.Model):
+    """
+    Model representing a category.
+    """
+    name = models.CharField('Категория', max_length=250, db_index=True)
+    parent = models.ForeignKey(
+        'Родительская категория',
+        'self',
+        on_delete=models.CASCADE,
+        related_name='children',
+        null=True,
+        blank=True
+    )
+    slug = models.SlugField('URL', max_length=250, unique=True, null=False, editable=True)
+    created_at = models.DateTimeField('Дата создания', auto_now_add=True)
+
+    class Meta:
+        unique_together = (['slug', 'parent'])
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        """
+        Returns a string representing the full path of the current node, including the names of all parent nodes in reverse order, separated by ' -> '.
+        """
+        full_path = [self.name]
+        k = self.parent
+        while k is not None:
+            full_path.append(k.name)
+            k = k.parent
+        return ' -> '.join(full_path[::-1])
+
+    def save(self, *args, **kwargs):
+        """
+        Save the object with the generated slug if it doesn't already have one.
+        """
+        if not self.slug:
+            self.slug = slugify(rand_slug() + '-pickBetter' + self.name)
+        super(Category, self).save(*args, **kwargs)
+    
+    # def get_absolute_url(self):
+    #     return reverse("model_detail", kwargs={"pk": self.pk})
+    
+    class Product(models.Model):
+        """
+        A model representing a product.
+        """
+        category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+        title = models.CharField('Название', max_length=250)
+        brand = models.CharField('Бренд', max_length=250)
+        description = models.TextField('Описание', blank=True)
+        slug = models.SlugField('URL', max_length=250)
+        price = models.DecimalField('Цена', max_digits=7, decimal_places=2, default=99.99)
+        image = models.ImageField('Изображение', upload_to='products/products/%Y/%m/%d/')
+        available = models.BooleanField('Наличие', default=True)
+        created_at = models.DataTimeField('Дата создания', auto_now_add=True)
+        updated_at = models.DataTimeField('Дата изменения', auto_now=True)
+
+        class Meta:
+            verbose_name = 'Продукт'
+            verbose_name_plural = 'Продукты'
+        
+        def __str__(self):
+            return self.title
+        
+        # def get_absolute_url(self):
+        #     return reverse("model_detail", kwargs={"pk": self.pk})
+
+
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        """
+        Return a filtered queryset containing only available products.
+        """
+        return super(ProductManager, self).get_queryset().filter(available=True)
+
+
+class ProductProxy(Product):
+
+    object = ProductManager()
+
+    class Meta:
+        proxy = True
